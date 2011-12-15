2623aa18addc8820396542ab159555c87180c423
Atsuki Takahashi <a1133at@aiit.ac.jp>
2011-12-15 04:18:01 -0800

    Deploy

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "myapp"
+set :deploy_to,  "/home/rails/myapp"
+
+set :repository, "git@github.com:a1133at/myapp"
+set :scm, :git
+
+set :use_sudo, false
+
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
